9ca6fceb491b831faba2b529c893d0d6b3ef8620
alescrocaro <alescrocaro@gmail.com>
2024-03-23 15:42:20 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# UID GENERATOR
+
+Simple uid generator inspired by [uuid generator](https://www.uuidgenerator.net).
+
+You can customize the uid you want by using the fields provided.
---
+++ src/app/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useEffect, useState } from "react";
+import { uid } from "uid";
+
+export default function Home() {
+  const [generatedUid, setGeneratedUid] = useState<string>();
+
+  function generateUID() {
+    setGeneratedUid(uid().toUpperCase());
+  }
+
+  useEffect(() => {
+    setGeneratedUid(uid().toUpperCase());
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+      <div className="flex flex-col items-center gap-4 group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
+        <span className="text-3xl font-bold">UID Generator</span>
+        <span className="text-2xl font-semibold">{generatedUid}</span>
+        <button
+          onClick={() => generateUID()}
+          className="rounded-lg border px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          generateUID
+        </button>
+      </div>
+
+      <div className="mb-16 flex items-center justify-end lg:max-w-9xl lg:w-full lg:mb-0 text-left">
+        <a
+          href="https://github.com/alescrocaro"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-md font-semibold">
+            Just coding what I need
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            <svg
+              height="32"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="32"
+              data-view-component="true"
+              className="octicon octicon-mark-github v-align-middle color-fg-default"
+            >
+              <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+            </svg>
+            alescrocaro
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
